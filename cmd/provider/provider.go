b29cce301eace1116a7b3423dabd7ee5ce392655
NewHouses <sergio.simons.c@gmail.com>
2025-03-17 18:45:46 +0200

    [OSP-93] create bus and connecting to kafka

--- cmd/provider/provider.go
+++ cmd/provider/provider.go
@@ -1,6 +1,10 @@
 package provider
 
-import "reactionservice/internal/api"
+import (
+	"reactionservice/infrastructure/kafka"
+	"reactionservice/internal/api"
+	"reactionservice/internal/bus"
+)
 
 type Provider struct {
 	env string
@@ -12,10 +16,32 @@ func NewProvider(env string) *Provider {
 	}
 }
 
+func (p *Provider) ProvideEventBus() (*bus.EventBus, error) {
+	kafkaProducer, err := kafka.NewKafkaProducer(p.kafkaBrokers())
+	if err != nil {
+		return nil, err
+	}
+
+	return bus.NewEventBus(kafkaProducer), nil
+}
+
 func (p *Provider) ProvideApiEndpoint() *api.Api {
 	return api.NewApiEndpoint(p.env, p.ProvideApiControllers())
 }
 
 func (p *Provider) ProvideApiControllers() []api.Controller {
 	return []api.Controller{}
+}
+
+func (p *Provider) kafkaBrokers() []string {
+	if p.env == "development" {
+		return []string{
+			"localhost:9093",
+		}
+	} else {
+		return []string{
+			"172.31.36.175:9092",
+			"172.31.45.255:9092",
+		}
+	}
 }
